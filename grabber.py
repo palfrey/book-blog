a110c17673d6bf9f401fb411b8d456c70437fc86
Tom Parker <palfrey@tevp.net>
2012-05-31 23:41:13 +0100

    Fix encoding and various bad character issues

--- grabber.py
+++ grabber.py
@@ -1,14 +1,27 @@
+# -*- coding: utf-8 -*-
 from urlgrab import Cache
 from blog_pb2 import All
 from re import compile, DOTALL, MULTILINE
 from os import mkdir, system
 from os.path import exists, join
 from hashlib import md5
+from codecs import open
+from sys import argv
+from urlparse import urljoin
 
 c = Cache()
 
 db = All()
 db.ParseFromString(open("series.list", "rb").read())
+
+wrong = {
+		u"â€œ": u"\"",
+		u"â€™": u"'",
+		u"â€": u"\"",
+		u"â€˜": u"'",
+		u"â€”": u" - ",
+		u"â€¦": u"-",
+		}
 
 for s in db.series:
 	if s.name in argv[1:]:
@@ -19,7 +32,7 @@ for s in db.series:
 			folder = "%s #%02d"%(s.description, index)
 			if not exists(folder):
 				mkdir(folder)
-			toc = open(join(folder, "toc.html"), "wb")
+			toc = open(join(folder, "toc.html"), "wb", "utf-8")
 			toc.write("""<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
 			<title>%s</title>
@@ -33,35 +46,41 @@ for s in db.series:
 			newitems = False
 			for x in range(20):
 				print "generating", page
-				data = c.get(page, max_age=-1).read()
-				open("dump", "wb").write(data)
+				p = c.get(page, max_age=-1)
+				data = p.read().decode("utf-8")
 
-				title = titlePattern.search(data)
-				assert title != None, page
-				title = title.groups()[0]
+				for k in wrong:
+					data = data.replace(k, wrong[k])
+
+				open("dump", "wb", "utf-8").write(data)
 
 				link = nextPattern.search(data)
 				fname = md5(page).hexdigest() + ".html"
 				fpath = join(folder, fname)
 
-				content = contentPattern.search(data)
-				assert content != None, page
-				content = content.groups()[0]
+				title = titlePattern.search(data)
+				assert title != None, page
+				title = title.groups()[0]
 
-				fname = md5(page).hexdigest() + ".html"
-				open (join(folder, fname), "wb").write("""<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-		<head>
-			<style type="text/css" title="override_css">
-				@page {padding: 0pt; margin:0pt}
-			</style>
-			<title>%s</title>
-		</head>
-		<body>
-			<h1>%s</h1>
-			%s
-		</body>
-	</html>"""%(title, title, content))
 				toc.write("\t\t\t<a title=\"%s\" href=\"%s\" />\n"%(title, fname))
+				if not exists(fpath):
+					newitems = True
+					content = contentPattern.search(data)
+					assert content != None, page
+					content = content.groups()[0]
+
+					open (fpath, "wb", "utf-8").write(u"""<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+			<head>
+				<style type="text/css" title="override_css">
+					@page {padding: 0pt; margin:0pt}
+				</style>
+				<title>%s</title>
+			</head>
+			<body>
+				<h1>%s</h1>
+				%s
+			</body>
+		</html>"""%(title, title, content))
 				if link != None:
 					link = link.groups()[0]
 				page = link
