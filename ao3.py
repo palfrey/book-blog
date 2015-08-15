0beb2da7dcb80d1051953d300162dbdf934d64c9
Tom Parker <palfrey@tevp.net>
2015-08-15 13:38:55 +0100

    Rewrite ao3.py as a one-shot script

--- ao3.py
+++ ao3.py
@@ -2,25 +2,44 @@ from sys import argv
 from urlgrab import Cache
 from codecs import open
 import re
+from common import *
+from urlparse import urljoin
 
 cache = Cache()
 url = argv[1]
-data = cache.get(url).read()
-#open("dump", "wb", "utf-8").write(data)
 
-title = re.search("<title>(.+?) - .+?</title>", data, re.MULTILINE | re.DOTALL)
-title = title.groups()[0].strip()
-author = re.search("rel=\"author\">([^<]+)</a>", data)
-author = author.groups()[0]
 id = re.search("/works/(\d+)", url)
 id = id.groups()[0]
 
-print """series {
-	name: "%s"
-	description: "%s"
-	author: "%s"
-	titlePattern: "<h2 class=\\"title heading\\">\\s+(.*?)\\s+</h2>"
-	startPage: "http://archiveofourown.org/works/%s/"
-	contentPattern: "<div[^>]+?class=\\"summary module\\"[^>]*?>(.*?)<!--/main-->"
-	nextPattern: "<a href=\\"(/works/\d+/chapters/\d+)\\">Next Chapter &#8594;</a>"
-}"""%(title.replace(" ",""), title, author, id)
+navigate = "http://archiveofourown.org/works/%s/navigate"%id
+print navigate
+
+data = cache.get(navigate).read()
+
+info = re.search("<h2 class=\"heading\">Chapter Index for <a href=\"/works/\d+\">([^<]+)</a> by <a href=\"[^\"]+\" class=\"login author\" rel=\"author\">([^<]+)</a></h2>", data)
+(title, author) = info.groups()
+
+titlePattern = re.compile("<h2 class=\"title heading\">\s+(.*?)\s+</h2>")
+summary = re.compile("<div[^>]+?class=\"summary module\"[^>]*?>(.+?)</div>", re.DOTALL|re.MULTILINE)
+notes = re.compile("<div.+?class=\"notes module\"[^>]*>(.+?)</div>", re.DOTALL|re.MULTILINE)
+mainContent = re.compile("<h3 class=\"landmark heading\" id=\"work\">Chapter Text</h3>(.*?)<!--/main-->", re.DOTALL|re.MULTILINE)
+volumePattern = re.compile("<li><a href=\"(/works/\d+/chapters/\d+)\">(\d+). ([^<]+)</a>")
+
+volumes = sorted(volumePattern.findall(data))
+
+print volumes
+volumes = dict([(int(x[1]), (x[0],x[2])) for x in volumes])
+print volumes
+
+toc = tocStart(title)
+
+for volumeUrl, volumeTitle in volumes.values():
+	print volumeUrl, volumeTitle
+	chapterPage = cache.get(urljoin(navigate, volumeUrl) + "?view_adult=true", max_age = -1).read()
+	items = [summary, notes, mainContent]
+	items = [x.search(chapterPage) for x in items]
+	items = [x.groups()[0] for x in items if x != None]
+	content = "\n".join(items)
+	generatePage(volumeUrl, volumeTitle, content, title, toc)
+tocEnd(toc)
+makeMobi(title, author)
